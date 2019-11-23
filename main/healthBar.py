0eb521b600aa637e73ada91d9ee8be471b63d40d
AnTiDoTe153 <calin.g.sulea@gmail.com>
2019-11-23 17:35:54 +0200

    [dev] Bigger health bar

--- main/healthBar.py
+++ main/healthBar.py
@@ -6,10 +6,10 @@ class HealthBar(Figure):
     damageColor = (255,69,0)
 
     def __init__(self, target, health):
-        width = target.width * 1.1
-        height = target.height // 10
+        width = target.width * 1.5
+        height = target.height * 0.25
         
-        pozY = target.pozY - (target.height // 2 + target.height * 0.2)
+        pozY = target.pozY - (target.height // 2 + target.height * 0.3)
         pozX = target.pozX
 
         super().__init__(target.map, pozX, pozY, width, height, HealthBar.healthColor)
@@ -23,8 +23,8 @@ class HealthBar(Figure):
         self.damageBar = self.initDamageBar()
 
     def initDamageBar(self):
-        damageWidth = ((self.maxHealth - self.currentHealth) * self.width) // self.maxHealth
-        damagePoz = self.pozX + self.width // 2 - damageWidth // 2
+        damageWidth = ((self.maxHealth - self.currentHealth) * self.width) / self.maxHealth
+        damagePoz = self.pozX + self.width / 2 - damageWidth / 2
         damageBar = Figure(self.map, damagePoz, self.pozY, damageWidth, self.height, HealthBar.damageColor)
         return damageBar
 
