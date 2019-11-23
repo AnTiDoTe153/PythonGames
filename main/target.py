d03ca6d3861279ecdabe24e87adda69be9da2323
AnTiDoTe153 <calin.g.sulea@gmail.com>
2019-11-23 16:05:50 +0200

    [fix] Fix issue with Target constructor

--- main/target.py
+++ main/target.py
@@ -9,15 +9,15 @@ class Target(Figure, Event):
     targetHeight = 30
     targetWidth = 30
 
-    def __init__(self, map, pozX, pozY):
-        Event.__init__()
-        Figure.__init__(map, pozX, pozY, Target.targetWidth, Target.targetHeight)
-
-    def __init__(self, map):
+    def __init__(self, map, pozX = None, pozY = None):
         Event.__init__(self)
-        pozX = random.randrange(0, map.width - Target.targetWidth)
-        pozY = random.randrange(0, map.height - Target.targetHeight)
+        if pozX == None:
+            pozX = random.randrange(0, map.width - Target.targetWidth)
+        if pozY == None:
+           pozY = random.randrange(0, map.height - Target.targetHeight)
+            
         Figure.__init__(self, map, pozX, pozY, Target.targetWidth, Target.targetHeight)
+
 
     def onCollision(self, figure):
         self.map.removeFigure(self)
