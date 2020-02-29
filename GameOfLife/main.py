9530d518a2caf1ec6692f598816fe30ff1dbb700
AnTiDoTe153 <calin.g.sulea@gmail.com>
2020-02-29 18:58:27 +0200

    [dev] Can draw grid in gameOfLife

--- GameOfLife/main.py
+++ GameOfLife/main.py
@@ -6,9 +6,8 @@ class Game:
 
         width = 700
         height = 700
-
-        self.screen = Screen(width, height)
-
+        self.grid = Grid(5, 5)
+        self.screen = Screen(width, height, self.grid)
 
     def play(self):
         while True:
@@ -17,16 +16,38 @@ class Game:
                 if event.type == pygame.QUIT:
                     return
             self.screen.update()
-        
+
+
+class Grid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.values = [[ 0 for j in range(width)] for i in range(height)]
+
 
 class Screen:
-    def __init__(self, width, height):
+    def __init__(self, width, height, grid):
         pygame.display.set_caption("Game of Life")
+        self.width = width
+        self.height = height
         self.screen = pygame.display.set_mode((width, height))
+        self.grid = grid
+
+    def drawGrid(self):
+        cellHeight = self.height / self.grid.height
+        cellWidth = self.width / self.grid.width
+
+        for i in range(1, self.grid.width):
+            pygame.draw.line(self.screen, (255,255,255), (cellWidth * i, 0), (cellWidth * i, self.height))
+
+        for i in range(1, self.grid.height):
+            pygame.draw.line(self.screen, (255,255,255), (0, cellHeight * i), (self.width, cellHeight * i))
 
 
     def update(self):
         self.screen.fill((0, 0, 0))
+        self.drawGrid()
         pygame.display.update()
 
 
